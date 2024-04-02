884bd1c5b56163cc28bf90aa3c4bb90419ab00a6
Marcelo Rodrigues <marcelo.rrodrigues21@gmail.com>
2024-04-02 15:03:30 -0300

    primeiro commit

--- Enunciados_Aula54.py
+++ Enunciados_Aula54.py
@@ -1,21 +1,24 @@
 """
+EXERCÍCIO 1-->
 Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
 
 """
+EXERCÍCIO 2-->
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
 """
+EXERCÍCIO 3-->
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
-'''
+
 #EXERCICIO 1
 numero = input('Digite um número inteiro: ')
 try:
@@ -59,7 +62,7 @@ if (hora_valida and minutos_validos and dois_pontos):
     elif (noite): print('Boa noite!')
 else: 
     print('\nhorário inválido')
-'''
+
 #EXERCÍCIO 3
 nome = input('Informe seu nome: ')
 if len(nome)<=4:
