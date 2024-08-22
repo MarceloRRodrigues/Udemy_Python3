654ab89d9983f2331303f2c5ea2df445683ab1c2
MarceloRRodrigues <marcelo.rrodrigues21@gmail.com>
2024-08-21 23:50:51 -0300

    update

--- Calc_While_Aula66.py
+++ Calc_While_Aula66.py
@@ -2,11 +2,12 @@ print('Bem-vindo à calculadora')
 while True:
     n1= input('Digite o primeiro número: ')
     n2= input('Digite o segundo número: ')
+    num1 = float(n1)
+    num2 = float(n2)
     if n1 is not float or n2 is not float:
         print("Numero(s) inválido(s). Entre com números reais.")
         continue
-    num1 = float(n1)
-    num2 = float(n2)
+    
     operacao= input('Digite a operacao (* + - /): ')
     if operacao=='*': print(num1*num2)
     elif operacao=='+': print(num1+num2)
